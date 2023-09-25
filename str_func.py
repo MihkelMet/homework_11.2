6caf159f7c98a00c7f7a4046c3b3b0e06cc89f45
Mihkel Metsa <mihkel992@gmail.com>
2023-09-25 11:04:31 -0700

    my new super functions

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def upper(word):
+    """great function"""
     return word.upper()
+
+def capitalize_words(string):
+    """cap letter all words"""
+    return ' '.join(s.capitalize() for s in string.split())
+
